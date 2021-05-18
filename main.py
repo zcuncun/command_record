cad6e8579d29a446ae3b9c3ad7231e50bdcf81e6
ZCC <zcuncun@outlook.com>
2021-05-18 10:41:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-from absl import flags, app, logging
+import argparse, logging
+import json
 
 
 def main(args):
-    del args
-    with open(FLAGS.config) as config_f:
+    with open(args.cfg) as config_f:
         config = json.load(config_f)
 
+        
 if __name__ == "__main__":
-    logging.set_verbosity(logging.DEBUG)
-    # logging.set_verbosity(logging.INFO)
-    FLAGS = flags.FLAGS
-    flags.DEFINE_string('config','default_config.json','配置文件')
-    app.run(main)
+    logging.setLevel(logging.DEBUG)    
+    parser = argparse.ArgumentParser(description='OCRTrain launch')
+    parser.add_argument('--cfg', required=True, type=str, help='config path')
+    args = parser.parse_args()
+    main(args)
